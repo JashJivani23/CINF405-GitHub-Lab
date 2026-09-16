208273afe2c6bdff2727e0a873445d57a76aab47
HethPatel21 <heth9270@gmail.com>
2026-09-15 20:14:40 -0400

    Update greeting as collaborator

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 def main():
     print('Hello from the CINF 405 GitHub lab!')
-
+    print('This is updated by Heth Patel as a Collaborator for this assignment')
 
 if __name__ == '__main__':
     main()
